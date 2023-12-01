31ede65a38f0041e39d32352df6c12ac294d51aa
yeonso08 <hjng0825@gmail.com>
2023-12-01 14:25:06 +0900

    feat : 푸터 추가

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="bg-gray-800 text-white text-center p-4">
+            <p>© {new Date().getFullYear()} HwangJaeYeon. All rights reserved.</p>
+        </footer>
+    );
+};
+
+export default Footer;

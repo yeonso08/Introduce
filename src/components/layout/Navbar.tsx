7eb1aa57268ee903d7f74369334d40c75d90a3e0
yeonso08 <hjng0825@gmail.com>
2023-12-01 14:22:46 +0900

    feat : 네브바 추가

---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+
+const Navbar = () => {
+    return (
+        <nav className='flex justify-between items-center h-16 bg-gray-800 text-white relative shadow-sm font-mono'>
+            <Link href='/'>
+                <div className='pl-8'>JaeYeon</div>
+            </Link>
+            <div className='px-4 cursor-pointer md:hidden'>
+                아이콘
+            </div>
+            <div className='pr-8 md:flex hidden'> {/* 클래스를 md:flex hidden으로 변경 */}
+                <Link href='/about'>
+                    <div className='p-4'>About</div>
+                </Link>
+                <Link href='/projects'>
+                    <div className='p-4'>Projects</div>
+                </Link>
+                <Link href='/contact'>
+                    <div className='p-4'>Contact</div>
+                </Link>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;

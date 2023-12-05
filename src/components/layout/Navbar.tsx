924f0b6d07ebcfe21410e5765b5e3e948009ab1b
yeonso08 <hjng0825@gmail.com>
2023-12-05 15:45:59 +0900

    design : projects 레이아웃 수정

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -4,20 +4,20 @@ const Navbar = () => {
     return (
         <nav className='flex justify-between items-center h-16 bg-black text-white fixed top-0 left-0 right-0 z-50 shadow-sm font-mono text-lg md:px-32'>
             <Link href='/'>
-                <div className='pl-8'>Jaeyeon Hwang</div>
+                <div className='pl-8 text-lg font-thin'>Jaeyeon Hwang</div>
             </Link>
             <div className='px-4 cursor-pointer md:hidden'>
                 아이콘
             </div>
             <div className='pr-8 md:flex hidden'>
                 <Link href='/about'>
-                    <div className='p-4'>About</div>
+                    <div className='p-4 text-lg font-thin'>About</div>
                 </Link>
                 <Link href='/projects'>
-                    <div className='p-4'>Projects</div>
+                    <div className='p-4 text-lg font-thin'>Projects</div>
                 </Link>
                 <Link href='/contact'>
-                    <div className='p-4'>Contact</div>
+                    <div className='p-4 text-lg font-thin'>Contact</div>
                 </Link>
             </div>
         </nav>
--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -4,6 +4,7 @@ const Projects = () => {
         <h2 className="text-4xl font-bold mb-4">Projects</h2>
         <div className="flex flex-col items-center justify-center">
           {/* Example of a single project entry */}
+          <div>
           <div className="flex items-center mb-8">
             {/* Project Image */}
             <div className="flex-none w-48 h-32 bg-gray-500 mr-4"> {/* Placeholder for image */}
@@ -12,15 +13,44 @@ const Projects = () => {
   
             {/* Project Description */}
             <div className="flex-grow">
-              <h3 className="text-2xl font-bold">Project Title</h3>
-              <p>Description of the project, technologies used, etc.</p>
+              <h3 className="text-2xl font-bold">링고스타</h3>
+              <p>외국인 상대로 한 렌터카 서비스</p>
               {/* Add more details or buttons/links as needed */}
             </div>
           </div>
+
+          <div className="flex items-center mb-8">
+            {/* Project Image */}
+            <div className="flex-none w-48 h-32 bg-gray-500 mr-4"> {/* Placeholder for image */}
+              {/* Insert <img> tag or background image here */}
+            </div>
+  
+            {/* Project Description */}
+            <div className="flex-grow">
+              <h3 className="text-2xl font-bold">Seniors</h3>
+              <p>6070분들의 재취업을 위한 프로젝트</p>
+              {/* Add more details or buttons/links as needed */}
+            </div>
+          </div>
+          <div className="flex items-center mb-8">
+            {/* Project Image */}
+            <div className="flex-none w-48 h-32 bg-gray-500 mr-4"> {/* Placeholder for image */}
+              {/* Insert <img> tag or background image here */}
+            </div>
+  
+            {/* Project Description */}
+            <div className="flex-grow">
+              <h3 className="text-2xl font-bold">킨더그루</h3>
+              <p>유치원이 아이의 교육에 보다 집중할 수 있도록</p>
+              <p>아이 관리를 지원하는 통합 관리 서비스</p>
+              {/* Add more details or buttons/links as needed */}
+            </div>
+          </div>
+
   
           {/* Repeat the above structure for each project */}
         </div>
-        
+        </div>
       </div>
     );
   };

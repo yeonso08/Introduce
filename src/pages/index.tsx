6cbd08c49913a5f7f13311bb832fd68de6941c8d
yeonso08 <hjng0825@gmail.com>
2024-03-20 10:09:54 +0900

    Revert "feat : 프레이머 모션 적용"
    This reverts commit a68f8cc970b8c047172eaa7dd2e2d7103b99df59.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,45 +1,45 @@
+import { useEffect, useState } from 'react';
+
 import About from '@/components/about/about';
 import Skills from '@/components/skills/skills';
 import Projects from '@/components/projects/projects';
 import Footer from '@/components/layout/Footer';
-import { motion } from "framer-motion"
-import Main from '@/components/main/Main';
 
 export default function Home() {
-  const sentence = "안녕하세요 프론트엔드 개발자 황재연 입니다.";
+  const [text, setText] = useState<string>('');
+  const fullText = '안녕하세요. 프론트엔드 개발자 황재연입니다.';
 
+  useEffect(() => {
+    let index: number = 0;
+    const timer = setInterval(() => {
+      setText(fullText.slice(0, index));
+      index++;
+      if (index > fullText.length) clearInterval(timer);
+    }, 200); // 타이핑 속도(밀리초 단위)
+
+    return () => clearInterval(timer);
+  }, []);
 
   return (
     <div className="relative h-screen">
       {/* 배경 이미지 */}
-      {/* <div className="absolute top-0 right-0 bottom-0 left-0 bg-cover bg-center"
-        style={{ backgroundImage: "url('/mainImage.png')" }}></div> */}
+      <div className="absolute top-0 right-0 bottom-0 left-0 bg-cover bg-center"
+        style={{ backgroundImage: "url('/mainImage.png')" }}></div>
 
       {/* 어두운 오버레이 */}
-      {/* <div className="absolute top-0 right-0 bottom-0 left-0 bg-black opacity-50"></div> */}
+      <div className="absolute top-0 right-0 bottom-0 left-0 bg-black opacity-50"></div>
 
       {/* 내용 (네비게이션 바, 푸터, 본문 등) */}
-      {/* <div className="relative z-10 flex justify-center items-center h-full">
-        {sentence.split('').map((char, index) => {
-          // 랜덤 시간을 생성합니다. (예: 1초에서 4초 사이)
-          const duration = Math.random() * 3 + 1;
-
-          return (
-            <motion.div
-              key={index}
-              animate={{ x: [-300, 0], rotate: 360 }}
-              transition={{ duration: duration }}
-              className="text-white text-6xl font-bold p-2">
-              {char}
-            </motion.div>
-          );
-        })}
-      </div> */}
-      <Main />
-      {/* <About />
+      <div className="relative z-10 flex justify-center items-center h-full">
+        {/* <p className="text-white text-6xl border-r-2 border-white p-2 animate-blink"> */}
+        <p className="text-white text-6xl font-bold p-2 animate-blink border-r-2 border-white">
+          {text}
+        </p>
+      </div>
+      <About />
       <Skills />
       <Projects />
-      <Footer /> */}
+      <Footer />
     </div>
   );
 }

d8312a43bd6c136376d7bd26ee45be89832a299d
황재연 <hjng0825@gmail.com>
2024-09-19 15:32:56 +0900

    chore : vercel analytics 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Navbar from '@/components/layout/Navbar'
+import {Analytics} from "@vercel/analytics/next";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+        <Analytics />
       <Navbar />
       <Component {...pageProps} />
     </>
